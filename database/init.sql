a6d2c08372f9869b28429dca4824ebf98468a24f
Salvador Rodriguez <chava.crb@gmail.com>
2025-11-25 15:46:01 -0600

    sql script wip

---
+++ database/init.sql
@@ -0,0 +1,16 @@
+-- Database initialization script for Cooking Assistant system
+
+-- Enable UUID extension
+CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
+
+-- Create ENUM types for better data integrity
+
+
+----------------------------------------------------
+-- TABLES
+----------------------------------------------------
+
+
+----------------------------------------------------
+-- INDEXES
+----------------------------------------------------
